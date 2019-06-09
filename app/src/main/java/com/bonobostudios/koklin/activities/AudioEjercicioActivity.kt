e2a4fcbb53a66a2d431408ee461d24481b89efa4
RodrigoCruz1154 <00115417@uca.edu.sv>
2019-06-09 12:14:58 -0600

    Vista historial. Vistas del ejercicio

---
+++ app/src/main/java/com/bonobostudios/koklin/activities/AudioEjercicioActivity.kt
@@ -0,0 +1,13 @@
+package com.bonobostudios.koklin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bonobostudios.koklin.R
+
+class AudioEjercicioActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audio_ejercicio)
+    }
+}
---
+++ app/src/main/java/com/bonobostudios/koklin/activities/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package com.bonobostudios.koklin.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HistoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
