9d3c9377c1376f202ba3f6e90b9f00fc60749c78
RodrigoCruz1154 <00115417@uca.edu.sv>
2019-06-10 11:28:53 -0600

    se agregan usuarios a firestore

--- app/src/main/java/com/bonobostudios/koklin/MainActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.bonobostudios.koklin
 
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    val TAG = ""
+
+    val db = FirebaseFirestore.getInstance()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        et4.setOnClickListener {
+            var nombre = et1.text.toString()
+
+            var mail = et2.text.toString()
+
+            var password = et3.text.toString()
+
+            val user = hashMapOf(
+                "email" to mail,
+                "name" to nombre,
+                "password" to password
+            )
+            db.collection("users").add(user)
+        }
+
     }
 }
