4673f9ee3d34b5b680120eea9a30505206f99c5a
RodrigoCruz1154 <00115417@uca.edu.sv>
2019-06-15 17:08:29 -0600

    Lee con eficacia los datos de la bd

--- app/src/main/java/com/bonobostudios/koklin/activities/MainActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/activities/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import com.bonobostudios.koklin.R
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,9 +35,19 @@ class MainActivity : AppCompatActivity() {
                                     L1Reference.collection("respuestas").document("respuesta$actual")
                                 respuestasReference.get().addOnSuccessListener { document ->
                                     val respuesta = document.get("respuesta").toString()
+                                    val esCorrecta = document.get("esCorrecta").toString()
                                     if (document != null) {
-                                        Log.d("PREGUNTA ", pregunta)
-                                        Log.d("RESPUESTA ", respuesta)
+                                        PreguntaID.setText(pregunta)
+
+                                        if ("respuesta$actual" == "respuesta1") {
+                                            respuesta1.setText(respuesta)
+                                        } else if ("respuesta$actual" == "respuesta2") {
+                                            respuesta2.setText(respuesta)
+                                        } else if ("respuesta$actual" == "respuesta3") {
+                                            respuesta3.setText(respuesta)
+                                        } else if ("respuesta$actual" == "respuesta4") {
+                                            respuesta4.setText(respuesta)
+                                        }
                                     } else {
                                         Log.w("ERROR!", "Algo pasó porque no estamos en vivo.")
                                     }
