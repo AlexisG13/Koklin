e343ecd8af045d780ab06523888556aba81c1dec
RodrigoCruz1154 <00115417@uca.edu.sv>
2019-06-15 18:58:01 -0600

    Validación y obtención hecha

--- app/src/main/java/com/bonobostudios/koklin/activities/MainActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.bonobostudios.koklin.activities
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -21,6 +22,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun ReadAllQuestions() {
+        var intentos = 3
         db.collection("nivel")
             .get()
             .addOnCompleteListener { task ->
@@ -41,12 +43,64 @@ class MainActivity : AppCompatActivity() {
 
                                         if ("respuesta$actual" == "respuesta1") {
                                             respuesta1.setText(respuesta)
+                                            respuesta1.setOnClickListener {
+                                                if(esCorrecta == "true"){
+                                                    respuesta1.setBackgroundColor(Color.argb(255,41,181,48))
+                                                }else{
+                                                    respuesta1.setBackgroundColor(Color.argb(255,181,41,48))
+                                                    intentos = intentos - 1
+                                                    IntentosRestantes.setText(intentos.toString())
+                                                    if(intentos==0){
+                                                        PreguntaID.setText("GAME OVER")
+                                                        respuesta1.setBackgroundColor(Color.argb(255,41,181,48))
+                                                    }
+                                                }
+                                            }
                                         } else if ("respuesta$actual" == "respuesta2") {
                                             respuesta2.setText(respuesta)
+                                            respuesta2.setOnClickListener {
+                                                if(esCorrecta == "true"){
+                                                    respuesta2.setBackgroundColor(Color.argb(255,41,181,48))
+                                                }else{
+                                                    respuesta2.setBackgroundColor(Color.argb(255,181,41,48))
+                                                    intentos = intentos - 1
+                                                    IntentosRestantes.setText(intentos.toString())
+                                                    if(intentos==0){
+                                                        PreguntaID.setText("GAME OVER")
+                                                        respuesta1.setBackgroundColor(Color.argb(255,41,181,48))
+                                                    }
+                                                }
+                                            }
                                         } else if ("respuesta$actual" == "respuesta3") {
                                             respuesta3.setText(respuesta)
+                                            respuesta3.setOnClickListener {
+                                                if(esCorrecta == "true"){
+                                                    respuesta3.setBackgroundColor(Color.argb(255,41,181,48))
+                                                }else{
+                                                    respuesta3.setBackgroundColor(Color.argb(255,181,41,48))
+                                                    intentos = intentos - 1
+                                                    IntentosRestantes.setText(intentos.toString())
+                                                    if(intentos==0){
+                                                        PreguntaID.setText("GAME OVER")
+                                                        respuesta1.setBackgroundColor(Color.argb(255,41,181,48))
+                                                    }
+                                                }
+                                            }
                                         } else if ("respuesta$actual" == "respuesta4") {
                                             respuesta4.setText(respuesta)
+                                            respuesta4.setOnClickListener {
+                                                if(esCorrecta == "true"){
+                                                    respuesta4.setBackgroundColor(Color.argb(255,41,181,48))
+                                                }else{
+                                                    respuesta4.setBackgroundColor(Color.argb(255,181,41,48))
+                                                    intentos = intentos - 1
+                                                    IntentosRestantes.setText(intentos.toString())
+                                                    if(intentos==0){
+                                                        PreguntaID.setText("GAME OVER")
+                                                        respuesta1.setBackgroundColor(Color.argb(255,41,181,48))
+                                                    }
+                                                }
+                                            }
                                         }
                                     } else {
                                         Log.w("ERROR!", "Algo pasó porque no estamos en vivo.")
