9cc717b25f1d9c37fe901892dc4032b5248f67d1
Alexis Gómez <00122717@uca.edu.sv>
2019-06-19 12:52:57 -0600

    Correción en la BD

--- app/src/main/java/com/bonobostudios/koklin/MainActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/MainActivity.kt
@@ -20,12 +20,15 @@ import com.firebase.ui.auth.IdpResponse
 import com.firebase.ui.auth.data.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.core.UserData
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+
+    val db = FirebaseFirestore.getInstance()
     private val REQUEST_CODE = 2019
     val auth = FirebaseAuth.getInstance()
 
@@ -67,6 +70,7 @@ class MainActivity : AppCompatActivity() {
                 var user = FirebaseAuth.getInstance().currentUser
                 if (user != null) {
                     if(!user.isEmailVerified){
+                        userExists(user)
                         verCorreo(user)
                     }
                     else{
@@ -74,7 +78,7 @@ class MainActivity : AppCompatActivity() {
                         finish()
                         return
                     }
-                    Toast.makeText(this,user.email,Toast.LENGTH_SHORT).show()
+                    //Toast.makeText(this,user.email,Toast.LENGTH_SHORT).show()
                 }
                 btn_sign_out.isEnabled = true
             }
@@ -83,6 +87,30 @@ class MainActivity : AppCompatActivity() {
 
     fun verCorreo(user: FirebaseUser){
         user.sendEmailVerification()
+    }
+
+    fun userExists(user:FirebaseUser){
+        val docIdRef = db.collection("users").document(user.uid)
+        docIdRef.get().addOnSuccessListener {document->
+            if(!document.exists()){
+                Toast.makeText(this,"se creo de nuevo",Toast.LENGTH_SHORT).show()
+                val userh = hashMapOf(
+                    "email" to user.email,
+                    "name" to user.displayName
+                )
+                db.collection("users").document(user.uid).set(userh)
+            }
+            else {
+                Toast.makeText(this,"Ya existe"+user.uid,Toast.LENGTH_SHORT).show()
+                var kk = db.collection("partida").whereEqualTo("user",user.uid).get()
+                kk.addOnSuccessListener { documents->
+                    for(document in documents){
+                        Log.d("TORTY","${document.data}")
+                    }
+                }
+            }
+            }
+        docIdRef.get()
     }
 
 }
