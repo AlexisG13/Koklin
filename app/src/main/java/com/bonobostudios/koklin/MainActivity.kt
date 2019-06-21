ac2aee4abc015688a27da3ca73b91ebf71ff0336
RodrigoCruz1154 <00115417@uca.edu.sv>
2019-06-21 16:23:15 -0600

    Trancisiones

--- app/src/main/java/com/bonobostudios/koklin/MainActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/MainActivity.kt
@@ -1,27 +1,18 @@
 package com.bonobostudios.koklin
 
 import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import com.bonobostudios.koklin.R
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
-import com.google.android.gms.tasks.Task
-import com.google.android.gms.common.api.ApiException
+import android.graphics.Color
+import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
-import com.firebase.ui.auth.data.model.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.core.UserData
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 
@@ -36,13 +27,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(com.bonobostudios.koklin.R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
         providers = Arrays.asList(
             AuthUI.IdpConfig.EmailBuilder().build(),
             AuthUI.IdpConfig.GoogleBuilder().build()
         )
 
+        actionProfile.setOnClickListener {
+            actionProfile.setBackgroundColor(Color.argb(255,178,178,178))
+            val intent : Intent = Intent(this,ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        actionSettings.setOnClickListener {
+            actionProfile.setBackgroundColor(Color.argb(255,178,178,178))
+            val intent : Intent = Intent(this,SettingsActivity::class.java)
+            startActivity(intent)
+        }
 
         showSignInOptions()
     }
@@ -72,7 +74,6 @@ class MainActivity : AppCompatActivity() {
                     }
                     //Toast.makeText(this,user.email,Toast.LENGTH_SHORT).show()
                 }
-                btn_sign_out.isEnabled = true
             }
         }
     }
---
+++ app/src/main/java/com/bonobostudios/koklin/ProfileActivity.kt
@@ -0,0 +1,19 @@
+package com.bonobostudios.koklin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_profile.*
+import java.util.*
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+    }
+
+}
