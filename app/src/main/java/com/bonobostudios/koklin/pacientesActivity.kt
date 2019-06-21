9c0a93c735f746583a69ea8b655bfa4c228da132
calderonra <00004017@uca.edu.sv>
2019-06-21 17:47:19 -0600

    falta obtener info

--- app/src/main/java/com/bonobostudios/koklin/pacientesActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/pacientesActivity.kt
@@ -3,16 +3,50 @@ package com.bonobostudios.koklin
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bonobostudios.koklin.Adapter.pacientesAdapter
+import com.bonobostudios.koklin.POJOS.paciente
+import kotlinx.android.synthetic.main.pacientes_activity.*
 
 class pacientesActivity: AppCompatActivity(){
 
+    private lateinit var  pacienteAdapter : pacientesAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+
+    private var pacienteList:ArrayList<paciente> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.pacientes_activity)
 
+
+        initRecyclerView()
 
 
 
     }
+
+    fun initRecyclerView(){
+        viewManager = LinearLayoutManager(this)
+        pacienteAdapter = pacientesAdapter(pacienteList,{pacienteIte: paciente-> pacienteIteClicked(pacienteIte)})
+
+        rv_pacientes.apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = pacienteAdapter
+        }
+
+    }
+
+    private fun pacienteIteClicked (item: paciente){
+        val pacienteBundle=Bundle()
+        pacienteBundle.putParcelable("PACIENTE",item)
+        //
+    }
+
+
+
 
 
 }
