99337e3f48fd7e60d692ba557f3e67ae0a715df3
Ra <00004017@uca.edu.sv>
2019-06-26 17:30:51 -0600

    Perfil de usuario y preguntas

--- app/src/main/java/com/bonobostudios/koklin/ProfileActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/ProfileActivity.kt
@@ -9,12 +9,36 @@ import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_profile.*
 import java.util.*
 
+
+private var name = ""
+private var email=""
 class ProfileActivity : AppCompatActivity() {
+    val rootRef = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        var us=FirebaseAuth.getInstance().currentUser?.uid
+        var datos= rootRef.collection("users").document(us!!)
+        datos.get().addOnSuccessListener {document->
+            if(document!=null){
 
+
+                name=document.get("name").toString()
+                email=document.get("email").toString()
+
+                NombreEspecialista.text= name
+                CargoEspecialista.text= email
+
+
+
+
+            }else{
+
+            }
+
+
+        }
         btn_sign_out.setOnClickListener{
             AuthUI.getInstance()
                 .signOut(this)
