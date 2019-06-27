28d12f09c337fd021964686b679a32e3b1eedd4e
RodrigoCruz1154 <00115417@uca.edu.sv>
2019-06-26 21:42:20 -0600

    :v

--- app/src/main/java/com/bonobostudios/koklin/MainActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.bonobostudios.koklin
 
-import android.app.Activity
+import android.app.ActionBar
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.Toast
@@ -10,23 +12,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bonobostudios.Adapter.EvaluacionAdapter
 import com.bonobostudios.Adapter.PacienteAdapter
-
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import android.text.Editable
-import android.text.TextWatcher
-
-
 
 
 class MainActivity : AppCompatActivity(), PacienteAdapter.OnPacienteSelectedListener,
@@ -43,6 +35,9 @@ class MainActivity : AppCompatActivity(), PacienteAdapter.OnPacienteSelectedList
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getSupportActionBar()?.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM)
+        getSupportActionBar()?.setCustomView(R.layout.action_bar_layout)
 
         query = rootRef.collection("pacientes ").whereEqualTo("user", FirebaseAuth.getInstance().currentUser?.uid)
 
