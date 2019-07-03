9c3f81acf49dbf752b53967f5889fe87f39d3ece
AlexisG13 <37200775+AlexisG13@users.noreply.github.com>
2019-07-03 11:59:49 -0600

    Comentando :o

--- app/src/main/java/com/bonobostudios/koklin/SettingsActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/SettingsActivity.kt
@@ -13,12 +13,14 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
+        
+        //Función para dirigir al usuario a la pagina de Privacy Policy
         tv_pp.setOnClickListener{
             val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://alexisg13.github.io/PrivacyPolicy"))
             startActivity(intent)
         }
 
+        //Funcion para regresar a la pantalla principal 
         InformacionViewClick.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
