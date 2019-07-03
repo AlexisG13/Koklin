d96f7db5b3c9f74da48af6ee3b439537ab2a6571
AlexisG13 <37200775+AlexisG13@users.noreply.github.com>
2019-07-03 11:44:37 -0600

    Comentando :)

--- app/src/main/java/com/bonobostudios/koklin/TestActivity.kt
+++ app/src/main/java/com/bonobostudios/koklin/TestActivity.kt
@@ -10,11 +10,13 @@ import java.util.*
 
 class TestActivity : AppCompatActivity()  {
 
+    //Declaración de variables y valores
     var nDoc =""
     var namePac : String ="N/A"
     val db = FirebaseFirestore.getInstance()
     var nPreguntas = 0
     var sIndex : Int = 0
+    //Array que contendra las sonidos que ya utilizamos
     var banned = arrayListOf<Int>()
     var nActual = 0
     lateinit var mainFragment: ExerciseFragment
@@ -28,6 +30,7 @@ class TestActivity : AppCompatActivity()  {
         initMainFragment()
     }
 
+    //Obtener un sonido al azar sin repetir
     fun getSonido():String{
         var flag = 0
         while(flag ==0){
@@ -41,23 +44,28 @@ class TestActivity : AppCompatActivity()  {
         return sound
     }
 
+    //Funcion para cambiar de fragmento
     fun changeFragment(id: Int, frag: Fragment){
         supportFragmentManager.beginTransaction()
             .setCustomAnimations(android.R.anim.slide_in_left,android.R.anim.slide_out_right)
             .replace(id, frag)
             .commit() }
 
+    //Funcion para quitar el fragmento del stack y asi no retroceder a el
     fun deleteFragment(frag: Fragment){
         supportFragmentManager.beginTransaction().remove(frag).commit()
     }
 
+    //Funcion para crear un nuevo fragmento y reemplazarlo con el anterior
     fun initMainFragment(){
         mainFragment = ExerciseFragment.newInstance(getSonido())
         var resource = R.id.main_fragment
         changeFragment(resource,mainFragment)
     }
 
+    //Funcion para insertar una evaluación
     fun insertEva(){
+        //Obtención de fecha y datos del paciente
         val date = SimpleDateFormat("dd-MM-yyyy").format(Date()).toString()
         val pacRef = db.collection("pacientes ").document(paciente)
         pacRef.get().addOnSuccessListener { document->
@@ -69,6 +77,7 @@ class TestActivity : AppCompatActivity()  {
                     "autor" to paciente,
                     "fecha" to date
                 )
+                //Inserción de la evaluación, en caso de exito llevar a la pantalla de detalle de evaluación
                 db.collection("evaluaciones ").add(mEv).addOnSuccessListener {document->
                     nDoc = document.id
                     val mIntent = Intent(this,EvDetailActivity::class.java)
